d5c1b380bf3a5aae6819d76a31fd58dda6000829
Rzn-Mohamed <meddrazin12@gmail.com>
2025-12-04 18:30:24 +0100

    tp18

---
+++ src/test/java/com/example/demo/tp18grpc/Tp18GRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.tp18grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp18GRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
